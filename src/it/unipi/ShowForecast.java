4af8d38c64fde994f5e187ceb45da3c08af7ca12
nebirhos <nebirhos@aol.com>
2011-11-30 10:03:02 +0100

    initial commit+

---
+++ src/it/unipi/ShowForecast.java
@@ -0,0 +1,15 @@
+package it.unipi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ShowForecast extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
